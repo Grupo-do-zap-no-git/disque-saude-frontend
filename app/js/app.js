21f2513b04a3ef0284cc69eaf8d2d66a4d7bbd1b
ArthurSampaio <arthur.sampaio.correia@ccc.ufcg.edu.br>
2017-08-27 17:53:41 -0300

    sempre fazer login do adm

--- app/js/app.js
+++ app/js/app.js
@@ -14,9 +14,17 @@
             .state("vs", {
                 abstract: true,
                 templateUrl: "view/main.html",
-                controller: "MainController as mainCtrl"
+                controller: "MainController as mainCtrl",
+                resolve : {
+                    login: function(){
+                        //sempre torna a obrigatoriedade de logar caso saia da rota do admin
+                        //SEGURANÇA ++
+                        return localStorage.clear();
+                    }
+                }
 
             })
+
             .state("admin", {
                 abstract: true,
                 url: "/admin",
@@ -106,7 +114,6 @@
                 }
             })
 
-
             .state("vs.sucesso", {
                 url: "/sucesso/:id",
                 views: {
@@ -117,19 +124,12 @@
                 }
             })
 
-
             .state("login", {
                 url: '/login',
                 templateUrl: 'view/login.html',
                 controller: 'LoginController as loginCtrl'
             })
 
-            .state("nao-autorizado", {
-                url: '/unauthorized',
-                templateUrl: 'view/naoAutorizado.html'
-
-
-            });
 
         $urlRouterProvider.otherwise('/');
         $locationProvider.html5Mode(false);
