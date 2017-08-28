6e3484c218f2e7d183981744926e1bd18143cbbf
tiagolimpe <tiago.pereira@ccc.ufcg.edu.br>
2017-08-27 21:14:52 -0300

    adicionando queixa do tipo animal

--- app/js/controller/RegistrarQueixas.controller.js
+++ app/js/controller/RegistrarQueixas.controller.js
@@ -1,12 +1,19 @@
 'use strict';
+var registraQueixaCtrl;
 (function() {
     var app = angular.module('vs');
 
     app.controller("RegistraQueixaCtrl", function RegistraQueixaCtrl(toastr, $state, QueixaService) {
 
-        var registraQueixaCtrl = this;
+        registraQueixaCtrl = this;
+
+        registraQueixaCtrl.tipoSelecionado = 'ANIMAL';
+        registraQueixaCtrl.animal = '';
+        registraQueixaCtrl.local = '';
 
         registraQueixaCtrl.registraQueixa = function registraQueixa(queixa) {
+            queixa.tipoDeQueixa = registraQueixaCtrl.tipoSelecionado;
+            addAtrr(queixa);
             QueixaService.registraQueixa(queixa)
                 .then(function success(response) {
                     toastr.success("Queixa adicionada com sucesso!");
@@ -14,6 +21,13 @@
                 }, function error() {
                     toastr.error("Problemas ao tentar adicionar queixa.");
                 });
+        }
+
+        function addAtrr(queixa) {
+            if(registraQueixaCtrl.tipoSelecionado === 'ANIMAL') {
+                queixa.tipoAnimal = registraQueixaCtrl.animal;
+                queixa.local = registraQueixaCtrl.local;
+            }
         }
     });
 })();
